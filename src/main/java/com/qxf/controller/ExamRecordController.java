b4a576e786b88748e2fcfc0361ed9829c8078fd3
xinfa Qiu <1816522516@qq.com>
2020-05-24 20:51:01 +0800

    完善查看考试记录

--- src/main/java/com/qxf/controller/ExamRecordController.java
+++ src/main/java/com/qxf/controller/ExamRecordController.java
@@ -4,15 +4,20 @@ import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.qxf.dto.ExamRecordDto;
+import com.qxf.entity.Exam;
 import com.qxf.entity.ExamRecord;
 import com.qxf.service.ExamRecordService;
+import com.qxf.service.ExamService;
 import com.qxf.util.EnumCode;
 import com.qxf.util.ResultUtil;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 考试记录表(ExamRecord)表控制层
@@ -29,6 +34,44 @@ public class ExamRecordController {
     @Resource
     private ExamRecordService examRecordService;
 
+    @Resource
+    private ExamService examService;
+
+    //查看考试详细结果
+    @GetMapping("/view")
+    public ResultUtil viewResult(String recordId){
+        //考试记录，包括了考生答案
+        ExamRecord examRecord = examRecordService.queryById(recordId);
+        //试题
+        Exam exam = examService.queryById(examRecord.getExamId());
+        //试题详情
+        Map<String, List<?>> examDetail = examService.getExamDetail(exam);
+        Map<String,Object> resultMap = new HashMap<>();
+        resultMap.put("questionMap",examDetail);
+        resultMap.put("exam",exam);
+        //解析答案
+        String fillAnswer = examRecord.getFillAnswer();
+        String judgeAnswer = examRecord.getJudgeAnswer();
+        String singleAnswer = examRecord.getSingleAnswer();
+        String multiAnswer = examRecord.getMultiAnswer();
+        // 解析答案
+        resolveAnswer(resultMap,fillAnswer,"fillAnswer");
+        resolveAnswer(resultMap,judgeAnswer,"judgeAnswer");
+        resolveAnswer(resultMap,singleAnswer,"singleAnswer");
+        resolveAnswer(resultMap,multiAnswer,"multiAnswer");
+        return new ResultUtil(EnumCode.OK.getValue(),"查看记录成功",resultMap);
+    }
+
+    // 解析答案
+    private void resolveAnswer(Map<String,Object> map,String answer,String answerKey){
+        if (!StringUtils.isEmpty(answer)){
+            //答题记录：题与题之间用*号分隔，同一个题的2个空之间用逗号分隔
+            String[] answers = answer.split("\\*");
+            map.put(answerKey,answers);
+        }else {
+            map.put(answerKey,"");
+        }
+    }
     /*
      * @Author qiuxinfa
      * @Description 添加考试记录
