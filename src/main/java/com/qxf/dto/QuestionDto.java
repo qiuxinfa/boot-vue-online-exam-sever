b6766b86ab37aa695e63ba9a7d07fb8f37201057
xinfa Qiu <1816522516@qq.com>
2020-05-26 23:14:21 +0800

    完成题库管理

--- src/main/java/com/qxf/dto/QuestionDto.java
+++ src/main/java/com/qxf/dto/QuestionDto.java
@@ -11,12 +11,19 @@ import java.util.Date;
  **/
 public class QuestionDto implements Serializable{
     private String id;
-    private String content;   //题目
+    private String questionContent;   //题目
+    private String questionAnswer;
+    private String choiceA;
+    private String choiceB;
+    private String choiceC;
+    private String choiceD;
     private Date createTime;
     private Date updateTime;
-    private String level;    //难度等级
-    private String typeStr;  //问题类型，字符串
-    private Integer type;    //问题类型，整形
+    private Integer questionLevel;       //难度等级
+    private String questionLevelStr;    //难度等级
+    private String questionExplain;
+    //问题类型（填空题，判断题，单选题，多选题），这个暂时没有用上，如果前端需要统一展示，则会用上
+    private String typeStr;
 
     public String getId() {
         return id;
@@ -26,12 +33,52 @@ public class QuestionDto implements Serializable{
         this.id = id;
     }
 
-    public String getContent() {
-        return content;
+    public String getQuestionContent() {
+        return questionContent;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent;
+    }
+
+    public String getQuestionAnswer() {
+        return questionAnswer;
+    }
+
+    public void setQuestionAnswer(String questionAnswer) {
+        this.questionAnswer = questionAnswer;
+    }
+
+    public String getChoiceA() {
+        return choiceA;
+    }
+
+    public void setChoiceA(String choiceA) {
+        this.choiceA = choiceA;
+    }
+
+    public String getChoiceB() {
+        return choiceB;
+    }
+
+    public void setChoiceB(String choiceB) {
+        this.choiceB = choiceB;
+    }
+
+    public String getChoiceC() {
+        return choiceC;
+    }
+
+    public void setChoiceC(String choiceC) {
+        this.choiceC = choiceC;
+    }
+
+    public String getChoiceD() {
+        return choiceD;
+    }
+
+    public void setChoiceD(String choiceD) {
+        this.choiceD = choiceD;
     }
 
     public Date getCreateTime() {
@@ -50,12 +97,28 @@ public class QuestionDto implements Serializable{
         this.updateTime = updateTime;
     }
 
-    public String getLevel() {
-        return level;
+    public Integer getQuestionLevel() {
+        return questionLevel;
     }
 
-    public void setLevel(String level) {
-        this.level = level;
+    public void setQuestionLevel(Integer questionLevel) {
+        this.questionLevel = questionLevel;
+    }
+
+    public String getQuestionLevelStr() {
+        return questionLevelStr;
+    }
+
+    public void setQuestionLevelStr(String questionLevelStr) {
+        this.questionLevelStr = questionLevelStr;
+    }
+
+    public String getQuestionExplain() {
+        return questionExplain;
+    }
+
+    public void setQuestionExplain(String questionExplain) {
+        this.questionExplain = questionExplain;
     }
 
     public String getTypeStr() {
@@ -64,13 +127,5 @@ public class QuestionDto implements Serializable{
 
     public void setTypeStr(String typeStr) {
         this.typeStr = typeStr;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
     }
 }
